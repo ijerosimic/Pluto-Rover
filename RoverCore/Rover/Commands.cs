c2c79e83f9ab19e4b28bb78d676aeb7f516e3c5b
Igor Jerosimic <igor.jerosimic@checkout.com>
2022-04-28 11:47:46 +0100

    Cleanup

--- RoverCore/Rover/Commands.cs
+++ RoverCore/Rover/Commands.cs
@@ -6,40 +6,4 @@ public static class Commands
     public const char Backward = 'B';
     public const char Right = 'R';
     public const char Left = 'L';
-
-    private static (int, int) MoveNorth((int X, int Y) currentPosition)
-    {
-        var (x, y) = currentPosition;
-        if (y > 99)
-            throw new ArgumentException("Command would move rover out of bounds.");
-
-        return new ValueTuple<int, int>(x, ++y);
-    }
-
-    private static (int, int) MoveSouth((int X, int Y) currentPosition)
-    {
-        var (x, y) = currentPosition;
-        if (y < 1)
-            throw new ArgumentException("Command would move rover out of bounds.");
-
-        return new ValueTuple<int, int>(x, --y);
-    }
-
-    private static (int, int) MoveEast((int X, int Y) currentPosition)
-    {
-        var (x, y) = currentPosition;
-        if (x > 99)
-            throw new ArgumentException("Command would move rover out of bounds.");
-
-        return new ValueTuple<int, int>(++x, y);
-    }
-
-    private static (int, int) MoveWest((int X, int Y) currentPosition)
-    {
-        var (x, y) = currentPosition;
-        if (x < 1)
-            throw new ArgumentException("Command would move rover out of bounds.");
-
-        return new ValueTuple<int, int>(--x, y);
-    }
 }
